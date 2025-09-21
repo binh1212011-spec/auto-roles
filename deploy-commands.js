011a1eda0c4137c66ec52adb976792b7ce94094a
binh1212011-spec <binh1212011@gmail.com>
2025-09-21 09:26:58 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,21 +7,17 @@ const commands = [
     new SlashCommandBuilder()
         .setName('verify')
         .setDescription('Verify Roblox account')
-].map(command => command.toJSON());
+].map(c => c.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
-
+        console.log('Refreshing commands...');
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
         );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
+        console.log('Commands reloaded.');
+    } catch (err) { console.error(err); }
 })();
